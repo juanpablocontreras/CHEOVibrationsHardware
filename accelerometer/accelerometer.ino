f6bd7bf621e72d0f78786a056a9afbfe9ceea503
juancontreras <juanpablocontreras@cmail.carleton.ca>
2020-03-10 17:31:08 -0400

    first impl of acc func

--- accelerometer/accelerometer.ino
+++ accelerometer/accelerometer.ino
@@ -1,12 +1,34 @@
 //Juan Pablo Contreras
 //Accelerometer code
 
-void setup() {
-  // put your setup code here, to run once:
 
+#include <MPU6050_tockn.h>
+#include <Wire.h>
+
+#define ACC_POLL_PERIOD 100 //poll accelerometer every ACC_POLL_PERIOD miliseconds
+
+MPU6050 mpu6050(Wire);
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+  mpu6050.begin();
+  mpu6050.calcGyroOffsets();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.print("\n");
 
+  for(int i=0; i<10;i++){
+   mpu6050.update();
+
+    Serial.print("temp : ");Serial.println(mpu6050.getTemp());
+    Serial.print("accX : ");Serial.print(mpu6050.getAccX());
+    Serial.print("\taccY : ");Serial.print(mpu6050.getAccY());
+    Serial.print("\taccZ : ");Serial.println(mpu6050.getAccZ());
+    Serial.print("\n");
+    delay(100);
+   }
+   delay(5000);//delay 5 seconds
 }
