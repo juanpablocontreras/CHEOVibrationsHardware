aa4a4239b72312dd30f787dbf512b55f284088ed
juancontreras <juanpablocontreras@cmail.carleton.ca>
2020-03-10 22:52:32 -0400

    get barometer to show something

--- Barometer/Barometer.ino
+++ Barometer/Barometer.ino
@@ -1,12 +1,28 @@
 //Juan Pablo Contreras
 //BMP180 Barometer
 
+#include <Wire.h>
+#include <Adafruit_BMP085.h>
+
+
+Adafruit_BMP085 bmp;
+
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  bmp.begin();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.print("Temperature = ");
+  Serial.print(bmp.readTemperature());
+  Serial.println(" *C");
 
+  Serial.print("Pressure = ");
+  Serial.print(bmp.readPressure());
+  Serial.println(" Pa");
+
+  Serial.println();
+  delay(2000);
 }
