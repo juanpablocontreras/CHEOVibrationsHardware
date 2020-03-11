1dc34be492bbc941134c64a544d6927b114a1213
juanpablocontreras <55557151+juanpablocontreras@users.noreply.github.com>
2020-03-11 11:06:42 -0400

    initial file for sound detection

---
+++ sound/sound.ino
@@ -0,0 +1,13 @@
+//Juan Pablo Contreras
+//sound detection
+
+
+void setup() {
+  // put your setup code here, to run once:
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
